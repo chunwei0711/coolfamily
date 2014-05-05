8841a9e855a6c6a2efdafdc050200b597e5d7219
chunwei <liuchunwei@coolchuan.net>
2014-05-05 15:14:18 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coolfamily::Application.config.secret_token = 'da99804b52831d31efba34a56d74ec31bd638b1c77846ab6d2ee5f2ec1abd1376048648d4660c3fa0f3ff9b62811f7e75a29be15e663893fe85b1904caf2f871'
